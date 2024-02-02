93794c5ba6ff8f39dac5d7e4e5b8169f4aab9202
Nathaniel White <natexornate@gmail.com>
2024-02-02 17:47:31 +0000

    Rename arguments to match parent.

--- zigpy_zboss/zigbee/application.py
+++ zigpy_zboss/zigbee/application.py
@@ -463,7 +463,7 @@ class ControllerApplication(zigpy.application.ControllerApplication):
             )
         )
 
-    def permit_with_link_key(self, node, code, time_s=60):
+    def permit_with_link_key(self, node, link_key, time_s=60):
         """Permit with link key."""
         raise NotImplementedError
 
