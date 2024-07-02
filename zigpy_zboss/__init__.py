24dd1bbc48b2f39fe2170b291bb4c06e490ff4ef
DamKas <48238600+DamKast@users.noreply.github.com>
2024-07-02 17:28:15 +0400

    bump version

--- zigpy_zboss/__init__.py
+++ zigpy_zboss/__init__.py
@@ -1,8 +1,8 @@
 """Init file for the zigpy_zboss package."""
 
 MAJOR_VERSION = 1
-MINOR_VERSION = 1
-PATCH_VERSION = 5
+MINOR_VERSION = 2
+PATCH_VERSION = 0
 
 __short_version__ = f"{MAJOR_VERSION}.{MINOR_VERSION}"
 __version__ = f"{__short_version__}.{PATCH_VERSION}"
