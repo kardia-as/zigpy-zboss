4ecd90b6882f28614493d4f50fb82e620debf83d
DamKas <48238600+DamKast@users.noreply.github.com>
2026-08-17 13:39:42 +0400

    fix request logging

--- zigpy_zboss/api.py
+++ zigpy_zboss/api.py
@@ -203,15 +203,13 @@ class ZBOSS:
             raise RuntimeError(
                 "Coordinator is disconnected, cannot send request")
 
-        LOGGER.debug("Sending request: %s", request)
-
         frame = request.to_frame()
         # If the frame is too long, it needs fragmentation.
         fragments = frame.handle_tx_fragmentation()
 
-        response_future = self.wait_for_response(request.Rsp(partial=True))
-
         async with self._conditional_blocking_request_lock(request.blocking):
+            LOGGER.debug("Sending request: %s", request)
+            response_future = self.wait_for_response(request.Rsp(partial=True))
             return await self._send_frags(
                 fragments, response_future, timeout=timeout)
 
